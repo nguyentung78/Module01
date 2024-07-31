faacdc614835bd17441eb123d39effb3c983685c
Nguyen Ba Tung <batungnguyen78@gmail.com>
2024-07-31 11:00:25 +0700

    hi

---
+++ session8/bai10.js
@@ -0,0 +1,9 @@
+let a = 6;
+let result = ++a + a + a++ + ++a + a;
+// reuslt = 7 + 7 + 7 + 9 + 9 =39
+// a sau khi thực hiện chương trình có giá trị = 9
+/**
+ * sự khác nhau giữa ++a và a++ là:
+ *      ++a sẽ tăng giá trị trước rồi sẽ in ra 
+ *      a++ sẽ in ra trước rồi mới tăng giá trị
+ */
--- session8/bai9.js
+++ session8/bai9.js
@@ -1,6 +1,6 @@
 let a = 5;
 let b= "5";
-console.log(a==b);  
-console.log(a===b);
-console.log(a!=b);  
-console.log(a!==b);
+console.log(a==b);  // true vì đây là so sánh giá trị của 2 biến a và b, khi tiến hành so sánh sẽ tự động chuyển đổi b từ string thành number, lúc đó a và b đều cùng giá trị và cùng kiểu dữ liệu nên sẽ trả về true
+console.log(a===b); // false vì đây là so sánh giá trị và kiểu dữ liệu, kiểu dữ liệu khác nhau, a là number còn b là string nên sẽ trả về false
+console.log(a!=b);  // false vì đây là so sánh giá trị, khi tiến hành so sánh sẽ tự động chuyển đổi kiểu dữ liệu của b về string nên a và b đều có cùng giá trị và kiểu dữ liệu, nên a khác b là sẽ trả về false
+console.log(a!==b); // true vì đây là so sánh giá trị và kiểu dữ liệu, kiểu dữ liệu khác nhau nên nó ko đúng cả về giá trị và kiểu dữ liệu nên sẽ trả về true
